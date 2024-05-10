948a3a3735f365afbc93b19568cafafe4d7913c4
enoch <ofori-antwi_enoch@bah.com>
2024-05-09 20:31:59 -0400

    updated automation.sh

--- automation.sh
+++ automation.sh
@@ -14,10 +14,5 @@ kubectl create ns jenkins
 kubectl apply -f jenkins-deployment.yaml -n jenkins
 kubectl apply -f jenkins-service.yaml -n jenkins
 
-# Step 3: Create Jenkins Pipeline
-# This step should involve Jenkins API calls or CLI commands to create the pipeline from Jenkinsfile in the repo
-
-# Step 4: Execute Jenkins Pipeline
-# Trigger Jenkins build for the microservice, either via Jenkins API or CLI
 
 echo "Setup complete!"
